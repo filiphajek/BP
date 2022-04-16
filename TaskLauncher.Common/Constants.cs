a00351c4fb43aec21b912508381c0e6a0a4d324d
wutshot <filiphajek268@gmail.com>
2022-04-16 02:34:06 +0200

    constants

--- TaskLauncher.Common/Constants.cs
+++ TaskLauncher.Common/Constants.cs
@@ -53,9 +53,9 @@ public static class Constants
     {
         public const string TokenBalance = "token_balance";
         public const string EmailVerified = "email_verified";
-        public const string Vip = "https://wutshot-test-api.com/vip";
-        public const string Registered = "https://wutshot-test-api.com/registered";
-        public const string Picture = "https://wutshot-test-api.com/picture";
+        public const string Vip = "https://bp-claims.com/vip";
+        public const string Registered = "https://bp-claims.com/registered";
+        public const string Picture = "https://bp-claims.com/picture";
     }
 
     /// <summary>
